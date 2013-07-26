f1522413b2a714c157b9b254fa1286c485cfa4b2
mjbondra <mjbondra@umich.edu>
2013-07-26 11:31:57 -0400

    add framework for dropping and seeding db

---
+++ db/data/index.js
@@ -0,0 +1,43 @@
+// About
+
+exports.about = [{
+  content: 'Q is a strategic design studio that has worked with hundreds of clients for over 30 years. We create brand strategies, visual systems, and marketing materials that deliver compelling user experiences in all media. We help our clients succeed and thrive by designing and developing successful communications. In turn and in our own small way, we help make the world a better place.'
+}];
+
+// Work
+
+exports.work = [{
+  title: 'applEcon',
+  description: 'New graphic identity, positioning, web site, and promotional materials for an Ann Arbor-based economics firm. Photography by Steve Kuzma.'
+},
+{
+  title: 'The Ark'
+},
+{
+  title: 'ACM SIGGRAPH'
+},
+{
+  title: 'The Kresge Foundation'
+},
+{
+  title: 'Café Zola'
+},
+{
+  title: 'One Pause Poetry'
+},
+{
+  title: 'Ann Arbor Area Community Foundation'
+},
+{
+  title: 'NuStep'
+},
+{
+  title: 'Wayne State University Press'
+},
+{
+  title: 'North Star Reach'
+},
+{
+  title: 'Hollander\'s'
+}];
+
