9a6519b21333cc12d95e4d5903d5e3e5c391a013
stolinski <scott.tolinski@gmail.com>
2013-07-30 14:28:52 -0400

    Adds Work dynamic project list

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,7 +5,8 @@
 
 var mongoose = require('mongoose')
   , Contact = mongoose.model('Contact')
-  , Panel = mongoose.model('Panel');
+  , Panel = mongoose.model('Panel')
+  , Project = mongoose.model('Project');
 
 /**
  * Index
@@ -16,11 +17,14 @@ exports.index = function (req, res) {
   var contact = ( contacts && contacts.length && contacts[contacts.length-1] ? contacts[contacts.length-1] : new Contact() );
   var panels = Panel.find({ parentView: 'home' }, function (err, panels) {
     if (err) return handleError(err);
-    return res.render('home', { 
-      title: 'Q Design & Communication Since 1981', 
-      onload: 'changevid()',
-      panels: res.locals.objectifyPanels(panels),
-      contact: contact
+    var projects = Project.find({}, function (err, projects) {
+      return res.render('home', { 
+        title: 'Q Design & Communication Since 1981', 
+        onload: 'changevid()',
+        panels: res.locals.objectifyPanels(panels),
+        projects: projects,
+        contact: contact
+      });
     });
   });  
 };
