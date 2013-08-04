391b698bac61c70554957ea0b19125720c2597ce
mjbondra <mjbondra@umich.edu>
2013-08-04 13:23:06 -0400

    use Q module for admin rendering home controller method

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -48,12 +48,27 @@ exports.index = function (req, res) {
  */
 
 exports.admin = function (req, res) {
-  var contacts = req.flash('contact');
-  var contact = ( contacts && contacts.length && contacts[contacts.length-1] ? contacts[contacts.length-1] : new Contact() );
-  res.render('home/admin', { 
-    title: 'Q Design & Communication Since 1981',
-    onload: 'changevid()',
-    contact: contact
-  });
+  Q.fcall(function () { 
+    var contacts = req.flash('contact');
+    return ( contacts && contacts.length && contacts[contacts.length-1] ? contacts[contacts.length-1] : new Contact() );
+  })
+    .then(function (contact) {
+      res.locals.contact = contact;
+      return Q.ninvoke(Panel.home, 'find');
+    })
+    .then(function (panels) { 
+      res.locals.panels = res.locals.objectifyPanels(panels); 
+      return Q.ninvoke(Project, 'find');
+    })
+    .then(function (projects) {
+      res.locals.projects = projects;
+      return res.render('home', { 
+        title: 'Q Design & Communication Since 1981', 
+        onload: 'changevid()'
+      });  
+    })
+    .fail(function (err) {
+      return res.render('500');
+    });
 };
 
