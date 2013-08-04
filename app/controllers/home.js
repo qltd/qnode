562033bab19d0881097c2240d482d8d8d0ebe332
mjbondra <mjbondra@umich.edu>
2013-08-03 22:15:52 -0400

    refactor and expand use of q module

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -12,26 +12,30 @@ var mongoose = require('mongoose')
 
 var Contact = mongoose.model('Contact')
   , Panel = mongoose.model('Panel')
-  , Project = mongoose.model('Project')
+  , Project = mongoose.model('Project');
 
 /**
  * Index
  */
 
 exports.index = function (req, res) {
-  var contacts = req.flash('contact');
-  var contact = ( contacts && contacts.length && contacts[contacts.length-1] ? contacts[contacts.length-1] : new Contact() );
-  Q.ninvoke(Panel, 'find', {})
+  Q.fcall(function () { 
+    var contacts = req.flash('contact');
+    return ( contacts && contacts.length && contacts[contacts.length-1] ? contacts[contacts.length-1] : new Contact() );
+  })
+    .then(function (contact) {
+      res.locals['contact'] = contact;
+      return Q.ninvoke(Panel, 'find', { parentView: 'home' });
+    })
     .then(function (panels) { 
       res.locals['panels'] = res.locals.objectifyPanels(panels); 
-      return Q.ninvoke(Project, 'find', {})
+      return Q.ninvoke(Project, 'find');
     })
     .then(function (projects) {
       res.locals['projects'] = projects;
       return res.render('home', { 
         title: 'Q Design & Communication Since 1981', 
-        onload: 'changevid()',
-        contact: contact
+        onload: 'changevid()'
       });  
     });
 };
